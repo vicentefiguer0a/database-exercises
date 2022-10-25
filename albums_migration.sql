6a4ed0428a23c62ef62f65f9064dd6d36d563eaa
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-10-25 12:10:21 -0500

    Tables: exercise completed

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(100),
+  name VARCHAR(100),
+  release_date DATE,
+  sales FLOAT,
+  genre VARCHAR(50),
+  PRIMARY KEY (id)
+);
