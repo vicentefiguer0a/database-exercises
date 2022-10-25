eeddf903f9f33bd21752207c34f5b5afa8881899
Vicente Figueroa <figueroa.vicente9@gmail.com>
2022-10-25 13:40:17 -0500

    Basic Statements: inserted data into codeup_test database

---
+++ albums_seeder.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop'),
+       ('AC/DC', 'Back in Black', 1980, 30.1, 'Hard Rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'R&B'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 28.7, 'Progressive Rock'),
+       ('Eagles', 'Hotel California', 1976, 31.8, 'Soft Rock'),
+       ('Shania Twain', 'Come On Over', 1997, 30.4, 'Country'),
+       ('Fleetwood Mac', 'Rumours', 1977, 29.3, 'Soft Rock'),
+       ('Bee Gees', 'Saturday Night Fever', 1977, 22.1, 'Disco');
